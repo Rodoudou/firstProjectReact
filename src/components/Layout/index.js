dfa03a8408f70bb9509a6e0954e4b72d7e8035ee
Redouane <Redouane@Redouane.local>
2020-11-25 01:54:03 +0100

    first commit

---
+++ src/components/Layout/index.js
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Layout, Menu, Modal, Image } from "antd";
+import MyCard from "../MyCard";
+import Tarifs from "../../containers/tarifs";
+import banner from "../../assets/img/banner.jpg";
+import SocialMedias from "../SocialMedias";
+import DarkModeToggle from "react-dark-mode-toggle";
+
+const { Header, Content, Footer } = Layout;
+
+const contentStyle = {
+  display: "flex",
+  flexWrap: "wrap",
+  padding: " 3rem 3rem",
+  justifyContent: "center",
+  // backgroundColor: "#6AC7FF",
+  footer: {
+    backgroundColor: "#001529",
+    color: "#FFFFFFA6",
+    display: "flex",
+    flexWrap: "wrap",
+    padding: " 3rem 3rem",
+    justifyContent: "center",
+  },
+};
+// Toglle Button for Dark Mode
+
+const AMRLayout = ({ children }) => {
+  // STATES
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [planingShow, setPlaningShow] = useState(false);
+  const [tarifShow, setTarifShow] = useState(false);
+
+  const fermerModaleHoraires = () => {
+    setPlaningShow(true);
+  };
+
+  const fermerModaleTarifs = () => {
+    setTarifShow(true);
+  };
+
+  // The toggle Dark Mode component
+  const Dark = () => {
+    return (
+      <DarkModeToggle onChange={setIsDarkMode} checked={isDarkMode} size={80} />
+    );
+  };
+
+  return (
+    <Layout className={isDarkMode ? "dark-mode" : "light-mode"}>
+      <Header className={isDarkMode ? "dark-mode" : "light-mode"}>
+        <Menu
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          theme={isDarkMode ? "dark" : "light"}
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+        >
+          <Menu.Item key="1">
+            <Link to="/">Accueil</Link>
+          </Menu.Item>
+          <Menu.Item onClick={fermerModaleHoraires} key="2">
+            Horaires
+          </Menu.Item>
+          <Menu.Item onClick={fermerModaleTarifs} key="3">
+            Tarifs
+          </Menu.Item>
+
+          <Menu.Item key="4">
+            <Link to="/inscription">Inscription</Link>
+          </Menu.Item>
+          <Menu.Item key="5">
+            <Link to="/contact">Contact</Link>
+          </Menu.Item>
+
+          <Menu.Item>
+            <SocialMedias />
+          </Menu.Item>
+          <Dark />
+        </Menu>
+
+        {/* Modal pour afficher les horraires */}
+        <Modal
+          title="Horaires des cours"
+          centered
+          visible={planingShow}
+          onOk={() => setPlaningShow(false)}
+          onCancel={() => setPlaningShow(false)}
+          width={500}
+        >
+          <MyCard />
+        </Modal>
+
+        {/* Modal pour afficher les Tarifs */}
+        <Modal
+          title="tarifs des cours"
+          centered
+          visible={tarifShow}
+          onOk={() => setTarifShow(false)}
+          onCancel={() => setTarifShow(false)}
+          width={500}
+        >
+          <Tarifs />
+        </Modal>
+      </Header>
+      <Image src={banner} fluid />
+      <Content style={contentStyle} children={children} />
+      <Footer style={contentStyle.footer}>
+        &#169;Create by Redouane Amrani with React 2020
+        <SocialMedias />
+      </Footer>
+    </Layout>
+  );
+};
+
+export default AMRLayout;
---
+++ src/containers/home/index.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import Article from "../../components/article";
+import Map from "../../components/map";
+
+
+function Home({isDarkMode}) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="container">
+    <div className="row">
+    <div className="app">
+      {isLoading ? (
+        <p>En chargement...</p>
+      ) : (
+        <section className="home-content" >
+          <Article isDarkMode={isDarkMode}/>
+          <Map />
+        </section>
+      )}
+    </div>
+    </div>
+</div>
+  );
+}
+
+export default Home;
